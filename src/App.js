386da7f0c817023ffbf1fdfa15d6a5cc8869778b
che-ri <rnjs1135@gmail.com>
2023-02-17 01:01:26 +0900

    [feat] App 예제 추가

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React from "react";
 
 function App() {
   const [cnt, setCnt] = React.useState(0);
+  const [list, setList] = React.useState([]);
 
   return (
     <div>
+      <h1>count up</h1>
       <span>{cnt}</span>
       <button onClick={() => setCnt(cnt + 1)}>+</button>
+      <div>
+        <h1>list</h1>
+        <button onClick={() => setList([...list, list.length])}>리스트 추가</button>
+        <ul>
+          {list.map((e) => (
+            <li key={`list-${e}`}>{e}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
