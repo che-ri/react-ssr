4abaf97d2343460f3d1deabcf09cacdf9f881ec6
che-ri <rnjs1135@gmail.com>
2023-02-17 16:00:54 +0900

    [feat] renderToPipeableStream 로직변경

--- renderToPipeableStream/server/app.js
+++ renderToPipeableStream/server/app.js
@@ -1,34 +1,26 @@
-import express from "express";
 import fs from "fs";
 import path from "path";
+import express from "express";
 import React from "react";
 import ReactDomServer from "react-dom/server";
-import App from "../src/App";
+import App from "../../renderToPipeableStream/src/App";
 
 const app = express();
 const PORT = 8000;
 
-app.use("^/$", (req, res) => {
-  let isError = false;
-
-  const stream = ReactDomServer.renderToPipeableStream(<App />, {
-    onAllReady() {
-      console.log("onAllReady");
-      isError = false;
-      res.statusCode = isError ? 500 : 200;
-      res.setHeader("content-type", "text/html;charset=utf-8");
-      stream.pipe(res);
-    },
-    onError(error) {
-      console.error(error);
-      isError = true;
-    },
-  });
-
-  setTimeout(() => stream.abort(), 10000);
-});
+const html = fs.readFileSync(path.resolve(__dirname, "../build/index.html"));
 
 app.use(express.static(path.resolve(__dirname, "..", "build")));
+
+app.use((req, res) => {
+  res.write(html);
+
+  const stream = ReactDomServer.renderToPipeableStream(<App />, {
+    onShellReady() {
+      stream.pipe(res);
+    },
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`server running on ${PORT} ::: http://localhost:${PORT}/`);
